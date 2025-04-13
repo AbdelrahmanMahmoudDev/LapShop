5f895d641207a5e8a5e6dfe2da969c32786fa73d
AbdelrahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2025-04-13 23:11:04 +0200

    Add project files.

---
+++ LapShop/Models/MainContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LapShop.Models
+{
+    public class MainContext : DbContext
+    {
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+            configurationBuilder.Properties<string>().HaveMaxLength(200);
+            configurationBuilder.Properties<decimal>().HaveColumnType("decimal(8, 2)");
+            configurationBuilder.Properties<DateTime>().HaveColumnType("DateTime");
+        }
+    }
+}
