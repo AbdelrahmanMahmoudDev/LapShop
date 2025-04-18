d8c7d891d1bb843eeaa5b85a9a2f461406658c72
AbdelrahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2025-04-19 00:57:08 +0200

    Added tests for CategoryRepository bound checks

---
+++ LapShop.Tests/CategoryRepositoryTests.cs
@@ -0,0 +1,86 @@
+﻿using LapShop.Models;
+using LapShop.Models.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+namespace LapShop.Tests
+{
+    public class CategoryRepositoryTests
+    {
+        // setting up mock data
+        private readonly MainContext _Context;
+        private readonly CategoryRepository _Repository;
+
+        public CategoryRepositoryTests()
+        {
+            _Context = new MainContext();
+            _Repository = new CategoryRepository(_Context);
+        }
+
+        [Fact]
+        public async Task Create_ObjIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            Category TestCategory = null;
+
+            // Act
+            async Task Act() => await _Repository.Create(TestCategory);
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(Act);
+        }
+
+        [Fact]
+        public async Task Delete_ObjIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            Category TestCategory = null;
+
+            // Act
+            async Task Act() => await _Repository.Delete(TestCategory);
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(Act);
+        }
+
+        [Fact]
+        public async Task GetAll_ObjIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            List<string> NavProps = null;
+            // Act
+            async Task Act() => await _Repository.GetAll(NavProps);
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(Act);
+        }
+
+        [Fact]
+        public async Task GetById_IdIsNegative_ThrowsArgumentNullException()
+        {
+            // Arrange
+            int id = -1;
+
+            // Act
+            async Task Act() => await _Repository.GetById(id);
+
+            // Assert
+            await Assert.ThrowsAsync<InvalidOperationException>(Act);
+        }
+
+        [Fact]
+        public async Task GetById_NavPropsIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            int id = 0;
+            List<string> NavProps = null;
+
+            // Act
+            async Task Act() => await _Repository.GetById(id, NavProps);
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(Act);
+        }
+    }
+}
